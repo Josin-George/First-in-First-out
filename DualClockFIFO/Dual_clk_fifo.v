b4076ead4e819e582ef01b09267a3762bc3b9245
Michael_j_nj <georgejosin6@gmail.com>
2025-10-28 02:24:42 +0530

    dual clock fifo

--- DualClockFIFO/Dual_clk_fifo.v
+++ DualClockFIFO/Dual_clk_fifo.v
@@ -1,6 +1,6 @@
-module FIFO(clk, rst, buf_in, buf_out, wr_en ,rd_en ,buf_empty ,buf_full, fifo_counter );
+module FIFO(clk_r, clk_w, rst, buf_in, buf_out, wr_en ,rd_en ,buf_empty ,buf_full, fifo_counter );
 
-input rst, clk, wr_en,rd_en;
+input rst, clk_r, clk_w, wr_en,rd_en;
 input [7:0] buf_in;
 output [7:0] buf_out ;
 output buf_empty ,buf_full;
@@ -19,21 +19,26 @@ begin
 
 end
 
-always@(posedge clk or posedge rst )
+always@(posedge clk_w or posedge rst )
 begin
     if(rst)
         fifo_counter <= 0;
-    else if((!buf_full && wr_en) && (!buf_empty && rd_en))//read and write simultaneous
-        fifo_counter <= fifo_counter;
     else if(!buf_full && wr_en )//writing
-        fifo_counter <= fifo_counter+1;
-    else if(!buf_empty && rd_en)//reading
-        fifo_counter <= fifo_counter-1;
-    else
-        fifo_counter <= fifo_counter;
+        fifo_counter <= fifo_counter + 1;
+  //  else
+  //      fifo_counter <= fifo_counter;
+end
+///two always block to avoid conflicting in the values of count
+always@(posedge clk_r)
+begin
+    
+    if(!buf_empty && rd_en)//reading
+        fifo_counter <= fifo_counter - 1;
+   // else
+   //     fifo_counter <= fifo_counter;
 end
 
-always@(posedge clk or posedge rst) 
+always@(posedge clk_r or posedge rst) 
     begin
         if(rst)
             buf_out <= 0;
@@ -47,7 +52,7 @@ always@(posedge clk or posedge rst)
     
     end
 
-always@(posedge clk)
+always@(posedge clk_w)
     begin
         if(wr_en && !buf_full)
             buf_mem[wr_ptr] <= buf_in;
@@ -55,25 +60,24 @@ always@(posedge clk)
             buf_mem[wr_ptr] <= buf_mem[wr_ptr];//else hold the data
     end
 
-always@(posedge clk or posedge rst)
-begin
-    if(rst )
-        begin
+always@(posedge clk_w or posedge rst)
+    begin
+        if(rst )
             wr_ptr <= 0;
+        else if(!buf_full && wr_en)
+            wr_ptr <= wr_ptr + 1;
+        else
+            wr_ptr <= wr_ptr;
+    end
+
+always@(posedge clk_r or posedge rst)
+    begin
+        if(rst )
             rd_ptr <= 0;
-        end
-    else
-        begin
-            //head pointer
-            if(!buf_full && wr_en)
-                wr_ptr <= wr_ptr + 1;
-            else
-                wr_ptr <= wr_ptr;
-            //tail pointer
-            if(!buf_empty && rd_en)
-                rd_ptr <= rd_ptr + 1;
-            else
-                rd_ptr <= rd_ptr;
-        end
-end
+        else if(!buf_empty && rd_en)
+            rd_ptr <= rd_ptr + 1;
+        else
+            rd_ptr <= rd_ptr;
+    end
+
 endmodule
